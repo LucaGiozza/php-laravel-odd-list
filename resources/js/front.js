1b3ac8fa8377f4484158c402c4a9a863a1466278
Luca Giozza <luca.giozza@gmail.com>
2021-09-20 21:16:05 +0200

    inizio vue router

--- resources/js/front.js
+++ resources/js/front.js
@@ -11,11 +11,13 @@
  
  import Vue from 'vue';
  import App from './views/App';
+ import router from './router';
 
 
  const app = new Vue({
 
     el: '#root',
-    render: h => h(App)
+    render: h => h(App),
+    router
 
  });
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Home</h1>
+        </div>
+    </div>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
